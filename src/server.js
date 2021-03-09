8560550865fdd2dacd4a96f9177683c6cc7622f2
Mycaell de Oliveira <oliveiramycaell@gmail.com>
2021-03-09 03:41:16 -0300

    Conclusão do back-end, término do último dia da NLW

--- src/server.js
+++ src/server.js
@@ -1,57 +1,21 @@
-const proffys = [
-    {
-        name: "Mycaell de Oliveira",
-        avatar: "https://avatars.githubusercontent.com/u/44411808?s=60&v=4",
-        whatsapp: "(81) 99951-9163",
-        bio: "Entusiasta das melhores tecnologias de química avançada. <br><br> Apaixonado por explodir coisas em laboratório.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-
-    {
-        name: "Mycaell de Oliveira",
-        avatar: "https://avatars.githubusercontent.com/u/44411808?s=60&v=4",
-        whatsapp: "(81) 99951-9163",
-        bio: "Entusiasta das melhores tecnologias de química avançada. <br><br> Apaixonado por explodir coisas em laboratório.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-]
-
-
-function pageLanding(req, res){
-    return res.render("index.html")
-}
-
-function pageStudy(req, res){
-    return res.render("study.html", {proffys})
-
-}
-
-function pageGiveClasses(req, res){
-    return res.render("give-classes.html")
-}
-
 const express = require('express')
 const server = express()
 const nunjucks = require('nunjucks')
+
+const {pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
 
 // configurando o nunjucks
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
-
 })
 
 
 // configurando o servidor
 server
+
+//receber os dados do req.body
+.use(express.urlencoded({extended: true}))
 
 // configurando arquivos estáticos
 .use(express.static("public"))
@@ -60,6 +24,7 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
-//porta aberta
+//porta utilizada
 .listen(5500)
